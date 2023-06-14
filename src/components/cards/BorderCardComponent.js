85f2d02b67f67f0d45879fd675fdebf001fc75a0
Yago Batista <yagobatistafis@yahoo.com.br>
2023-06-13 21:19:40 -0300

    update: bordercard colors

--- src/components/cards/BorderCardComponent.js
+++ src/components/cards/BorderCardComponent.js
@@ -1,22 +1,8 @@
-import { Flex, Grid } from "@chakra-ui/react";
+import { Flex, Grid, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useControls } from "leva";
 
 function BorderCard({ controler }) {
-  const cardVariant = {
-    rest: {
-      y: 0,
-      boxShadow: "none",
-    },
-    hover: {
-      boxShadow: `0px 0px 30px 1px ${controler.ShadowColor}`,
-      y: -10,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-
   const innerCardVariant = {
     rest: {
       scale: 1,
@@ -53,7 +39,7 @@ function BorderCard({ controler }) {
         backgroundImage={`linear-gradient(163deg, ${controler.ColorOne} 0%, ${controler.ColorTwo} 100%)`}
         initial={{ y: 0, boxShadow: "none" }}
         whileHover={{
-          boxShadow: `0px 0px 30px 1px ${controler.ShadowColor}`,
+          boxShadow: `0px 0px 20px 1px ${controler.ShadowColor}`,
           y: -10,
           transition: { duration: 0.2 },
         }}
@@ -62,12 +48,16 @@ function BorderCard({ controler }) {
           as={motion.div}
           w="100%"
           h="254px"
-          bg="#1a1a1a"
+          bg={controler.BackgroundColor}
           variants={innerCardVariant}
           initial="rest"
           whileHover="hover"
+          align="center"
+          justify="center"
         >
-          olá
+          <Text fontSize="28px" color={controler.TextColor}>
+            Hover me
+          </Text>
         </Flex>
       </Flex>
     </Flex>
@@ -79,6 +69,8 @@ export default function BorderCardComponent() {
     ShadowColor: "rgba(0, 255, 117, 0.30)",
     ColorOne: "#00ff75",
     ColorTwo: "#3700ff",
+    BackgroundColor: "#1e1e6c",
+    TextColor: "#53d12c",
     Amount: { value: 6, min: 1, max: 20, step: 1 },
     ContainerWidth: { value: "100%", max: "100%" },
   });
@@ -86,15 +78,15 @@ export default function BorderCardComponent() {
   return (
     <Flex direction="column" gap="20px" w="100%">
       <Flex w="100%" align="center" justify="center">
-        <Flex w={controler.ContainerWidth} border="1px solid white" p="15px">
+        <Flex w={controler.ContainerWidth} border="1px solid white" p="35px">
           <Grid
             templateColumns="repeat(auto-fill, minmax(190px, 1fr))"
-            gap="20px"
+            gap="30px"
             w="100%"
           >
             {Array.from({ length: controler.Amount }).map((item, index) => {
               return (
-                <Flex as={motion.div} whileHover={{ y: -10 }}>
+                <Flex as={motion.div} whileHover={{ y: -5 }}>
                   <BorderCard key={index} controler={controler} />
                 </Flex>
               );
