0d61404d41f4cb9baa6dd9948beca6559a554143
Yago Batista <yagobatistafis@yahoo.com.br>
2023-10-25 02:05:17 -0300

    fix: simple carousel

--- src/components/animations/SimpleCarousel.js
+++ src/components/animations/SimpleCarousel.js
@@ -21,15 +21,28 @@ const SimpleCarousel = () => {
       x: "0",
       opacity: 1,
       transition: {
-        duration: 1,
+        x: {
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+        },
+        opacity: {
+          duration: 0.5,
+        },
       },
     },
-    exit: {
+    exitRight: {
+      x: "100%",
       opacity: 0,
-      //   scaleY: 0.3,
-      //   scaleX: 0.8,
       transition: {
-        duration: 0.3,
+        duration: 0.2,
+      },
+    },
+    exitLeft: {
+      x: "-100%",
+      opacity: 0,
+      transition: {
+        duration: 0.2,
       },
     },
   };
@@ -65,7 +78,6 @@ const SimpleCarousel = () => {
 
   const handlePrevious = () => {
     setDirection("left");
-
     setCurrentIndex((prevIndex) =>
       prevIndex - 1 < 0 ? test.length - 1 : prevIndex - 1
     );
@@ -86,7 +98,7 @@ const SimpleCarousel = () => {
           position="relative"
           overflow="hidden"
         >
-          <AnimatePresence>
+          <AnimatePresence mode="wait">
             <Flex
               as={motion.div}
               w="400px"
@@ -98,7 +110,7 @@ const SimpleCarousel = () => {
               key={currentIndex}
               initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
               animate="visible"
-              exit="exit"
+              exit={direction === "right" ? "exitLeft" : "exitRight"}
               variants={slideVariants}
             >
               {test[currentIndex]}
@@ -124,14 +136,7 @@ const SimpleCarousel = () => {
                 whileHover="hover"
                 onClick={handlePrevious}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  viewBox="0 96 960 960"
-                  width="20"
-                >
-                  <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-                </svg>
+                {/* Left arrow SVG */}
               </Flex>
 
               <Flex
@@ -152,14 +157,7 @@ const SimpleCarousel = () => {
                 whileHover="hover"
                 onClick={handleNext}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  viewBox="0 96 960 960"
-                  width="20"
-                >
-                  <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-                </svg>
+                {/* Right arrow SVG */}
               </Flex>
             </Flex>
           </div>
@@ -171,7 +169,7 @@ const SimpleCarousel = () => {
               w="20px"
               h="20px"
               borderRadius="50px"
-              bg="green"
+              bg="blue"
               as={motion.div}
               key={index}
               onClick={() => handleDotClick(index)}
@@ -186,4 +184,5 @@ const SimpleCarousel = () => {
     </div>
   );
 };
+
 export default SimpleCarousel;
