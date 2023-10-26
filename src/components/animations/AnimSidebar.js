ab16c028ced5d08661852699221ecd14481eac83
Yago Batista <yagobatistafis@yahoo.com.br>
2023-10-26 17:48:21 -0300

    update: sidebar is working

--- src/components/animations/AnimSidebar.js
+++ src/components/animations/AnimSidebar.js
@@ -13,26 +13,43 @@ const variants = {
 };
 
 const textVariants = {
-  open: { opacity: 1, transition: { delay: 0.5, duration: 1 } },
-  closed: { opacity: 0 },
-};
-
-const btnVariants = {
   open: {
-    justify: "space-between",
+    opacity: 1,
+    display: "block",
     transition: { delay: 0.5, duration: 1 },
   },
-  close: {
-    justify: "center",
-    transition: { delay: 0.5, duration: 1 },
-  },
+  closed: { opacity: 0, display: "none" },
 };
+// const textVariants = {
+//   open: { opacity: 1, width: "auto", transition: { delay: 0.5, duration: 1 } },
+//   closed: { opacity: 0, width: 0, transition: { duration: 0 } },
+// };
 
 const AnimSidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <Flex w="500px" bg="orange">
-      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+    <Flex w="500px" direction="column" bg="#010914">
+      <Flex justify="center" align="center" w="100%" h="70px">
+        <Text>Header</Text>
+      </Flex>
+      <Flex w="100%">
+        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Flex w="100%" p="15px">
+          <Flex
+            w="100%"
+            border="1px solid white"
+            align="center"
+            justify="center"
+            onClick={() => setIsOpen((prev) => !prev)}
+            cursor="pointer"
+            direction="column"
+            gap="20px"
+          >
+            <Text>{isOpen ? "Close Sidebar" : "Open Sidebar"}</Text>
+            <Text>Your component goes here</Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
@@ -43,15 +60,14 @@ function Sidebar({ isOpen, setIsOpen }) {
       as={motion.ul}
       direction="column"
       position="sticky"
+      px="8px"
       top="70px"
       gap="10px"
       animate={isOpen ? "open" : "closed"}
       variants={variants}
-      //   h="calc(100vh - 70px)"
       w="240px"
-      bg="pink"
+      bg="#010914"
     >
-      {/* <Flex onClick={() => setIsOpen((prev) => !prev)}>olá</Flex> */}
       {tags.map((tag, index) => {
         return (
           <SidebarButton
@@ -81,38 +97,27 @@ function SidebarButton({ isOpen, setIsOpen, textVariants, text, index }) {
     >
       <Flex
         as={motion.div}
-        // whileHover={{
-        //   x: 15,
-        //   scale: 1.1,
-        //   transition: {
-        //     duration: 0.2,
-        //     ease: "linear",
-        //   },
-        // }}
-        // animate={isOpen ? "open" : "closed"}
-        // variants={btnVariants}
         h="50px"
         borderRadius="10px"
+        gap="13px"
+        w="100%"
         px="15px"
         bg="transparent linear-gradient(180deg, #8B98A9 0%, #0F161C 100%) 0% 0% no-repeat padding-box"
-        // justify={isOpen ? "space-between" : "center"}
-        justify="space-between"
+        justify="left"
         align="center"
       >
+        <Flex h="18px" w="18px" align="center" justify="center">
+          <AiFillGithub size="100%" />
+        </Flex>
         <Text
           as={motion.div}
           animate={isOpen ? "open" : "closed"}
           textTransform="capitalize"
           variants={textVariants}
           fontSize="15px"
-          //   fontWeight={router.query.tag === url ? 600 : 400}
-          //para remover o texto rapidamente quando a sidebar fecha \/\/\/\/
-          display={isOpen ? "flex" : "none"}
-          //   color={router.query.tag === url ? "white" : "#ffffffa4"}
         >
           {text}
         </Text>
-        <AiFillGithub height="50px" width="50px" />
       </Flex>
     </li>
   );
