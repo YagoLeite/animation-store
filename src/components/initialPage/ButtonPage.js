b792f4104c28134061c87477e120734959868a4a
Yago Batista <yagobatistafis@yahoo.com.br>
2023-11-03 16:47:38 -0300

    fix: pagination on url

--- src/components/initialPage/ButtonPage.js
+++ src/components/initialPage/ButtonPage.js
@@ -5,7 +5,7 @@ import React from "react";
 
 const ButtonPage = () => {
   return (
-    <Link href={"/all"} _hover={{ textDecoration: "none" }}>
+    <Link href={"/all?page=1"} _hover={{ textDecoration: "none" }}>
       <Flex
         as={motion.div}
         whileHover={{ boxShadow: "0px 0px 5px #64ffda", y: -2 }}
