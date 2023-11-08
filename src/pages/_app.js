09661a9defc3eaa22685d102b7a24562854b2967
Yago Batista <yagobatistafis@yahoo.com.br>
2023-11-08 03:35:58 -0300

    add: vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,12 +4,14 @@ import theme from "@/theme/theme";
 import "@fontsource/inter";
 import "@fontsource/lekton";
 import { ProgressProvider } from "@/context/ProgressContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <ProgressProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ProgressProvider>
     </ChakraProvider>
   );
