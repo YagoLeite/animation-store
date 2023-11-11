744d5b38ab205a7b02172b8993132bb3cd7e318f
Yago Batista <yagobatistafis@yahoo.com.br>
2023-11-11 20:10:21 -0300

    update: bouncing loader ready

--- src/components/animations/BouncingLoader.js
+++ src/components/animations/BouncingLoader.js
@@ -1,16 +1,21 @@
 import { Flex } from "@chakra-ui/react";
 import { motion, useAnimation } from "framer-motion";
 import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const CodePlayground = dynamic(() => import("../code/CodePlayground"), {
+  ssr: false,
+});
 
 const movementVariant = {
   start: {
-    top: ["-5%", "50%", "45%", "50%", "45%", "50%"], // Start above the container, then move to the center
-    left: ["50%", "50%", "50%", "50%", "50%"], // Center horizontally
-    x: ["-50%", "-50%", "-50%", "-50%", "-50%"], // Center the element horizontally from the beginning
-    y: ["-50%", "-50%", "-50%", "-50%", "-50%"], // Apply only when expansion starts to keep the element centered
-    width: ["40px", "40px", "40px", "40px", "1000px"], // Grow to full width
-    height: ["40px", "40px", "40px", "40px", "400px"], // Grow to full height
-    borderRadius: ["50%", "50%", "50%", "50%", "0%"], // Transition from circle to square
+    top: ["-5%", "50%", "45%", "50%", "45%", "50%"],
+    left: ["50%", "50%", "50%", "50%", "50%"],
+    x: ["-50%", "-50%", "-50%", "-50%", "-50%"],
+    y: ["-50%", "-50%", "-50%", "-50%", "-50%"],
+    width: ["40px", "40px", "40px", "40px", "1000px"],
+    height: ["40px", "40px", "40px", "40px", "400px"],
+    borderRadius: ["50%", "50%", "50%", "50%", "0%"],
     transition: {
       duration: 2,
       ease: "easeInOut",
@@ -19,17 +24,167 @@ const movementVariant = {
     },
   },
   rewind: {
-    top: ["50%", "45%", "50%", "-5%"], // Reverse the keyframes from the 'start' variant
+    top: ["50%", "45%", "50%", "-5%"],
     left: "50%",
     x: "-50%",
     y: "-50%",
-    width: ["1000px", "40px", "40px", "40px"], // Reverse width and height expansion
+    width: ["1000px", "40px", "40px", "40px"],
     height: ["400px", "40px", "40px", "40px"],
-    borderRadius: ["0%", "50%", "50%", "50%"], // Reverse borderRadius change
+    borderRadius: ["0%", "50%", "50%", "50%"],
     transition: {
       duration: 2,
       ease: "easeInOut",
-      times: [1, 0.52, 0, 5, 0.48, 0], // Reverse the 'times' as well
+      times: [1, 0.52, 0, 5, 0.48, 0],
+    },
+  },
+};
+
+const contentVariant = {
+  initial: {
+    opacity: 0,
+    transition: { duration: 0 },
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 2, duration: 1 },
+  },
+  reset: {
+    opacity: [0, 0, 1],
+    transition: { duration: 1, delay: 2, times: [0, 0, 1] },
+  },
+  rewind: {
+    opacity: 0,
+  },
+};
+
+const BouncingLoader = ({ isCoding }) => {
+  const controls = useAnimation();
+  const contentControls = useAnimation();
+
+  useEffect(() => {
+    controls.start("start");
+    contentControls.start("visible");
+  }, [isCoding]);
+
+  const resetHandler = () => {
+    contentControls.start("reset");
+    controls.start("start");
+  };
+
+  const rewindHandler = async () => {
+    contentControls.start("rewind");
+    await controls.start("rewind");
+    controls.start("start");
+    contentControls.start("visible");
+  };
+
+  return (
+    <>
+      {isCoding ? (
+        <CodePlayground code={code()} />
+      ) : (
+        <Flex
+          w="100%"
+          maxW="1000px"
+          h="100%"
+          minH="400px"
+          bg="pink"
+          position="relative"
+          overflow="hidden"
+          align="center"
+          justify="center"
+        >
+          <Flex
+            as={motion.div}
+            variants={movementVariant}
+            initial="start"
+            animate={controls}
+            w="40px"
+            h="40px"
+            position="absolute"
+            top="0"
+            left="0"
+            borderRadius="50%"
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+          />
+          <Flex
+            as={motion.div}
+            variants={contentVariant}
+            initial="initial"
+            animate={contentControls}
+            zIndex={1}
+            direction="column"
+            align="center"
+            justify="center"
+            gap="20px"
+          >
+            <Flex
+              as={motion.div}
+              whileHover={{ scale: 1.1, color: "black" }}
+              onClick={resetHandler}
+              w="150px"
+              h="60px"
+              align="center"
+              justify="center"
+              border="1px solid"
+              cursor="pointer"
+            >
+              Reset
+            </Flex>
+            <Flex
+              as={motion.div}
+              whileHover={{ scale: 1.1, color: "black" }}
+              onClick={rewindHandler}
+              w="150px"
+              h="60px"
+              align="center"
+              justify="center"
+              border="1px solid"
+              cursor="pointer"
+            >
+              Rewind
+            </Flex>
+          </Flex>
+        </Flex>
+      )}
+    </>
+  );
+};
+
+function code() {
+  return `
+import { Flex } from "@chakra-ui/react";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect } from "react";
+
+const movementVariant = {
+  start: {
+    top: ["-5%", "50%", "45%", "50%", "45%", "50%"],
+    left: ["50%", "50%", "50%", "50%", "50%"],
+    x: ["-50%", "-50%", "-50%", "-50%", "-50%"],
+    y: ["-50%", "-50%", "-50%", "-50%", "-50%"],
+    width: ["40px", "40px", "40px", "40px", "1000px"],
+    height: ["40px", "40px", "40px", "40px", "400px"],
+    borderRadius: ["50%", "50%", "50%", "50%", "0%"],
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      times: [0, 0.48, 0.5, 0.52, 1],
+      repeatDelay: 1,
+    },
+  },
+  rewind: {
+    top: ["50%", "45%", "50%", "-5%"],
+    left: "50%",
+    x: "-50%",
+    y: "-50%",
+    width: ["1000px", "40px", "40px", "40px"],
+    height: ["400px", "40px", "40px", "40px"],
+    borderRadius: ["0%", "50%", "50%", "50%"],
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      times: [1, 0.52, 0, 5, 0.48, 0],
     },
   },
 };
@@ -104,19 +259,44 @@ const BouncingLoader = () => {
         initial="initial"
         animate={contentControls}
         zIndex={1}
-        w="90%"
-        h="90%"
-        border="1px solid"
         direction="column"
         align="center"
         justify="center"
-        gap="10px"
+        gap="20px"
       >
-        <Flex onClick={resetHandler}>Reset</Flex>
-        <Flex onClick={rewindHandler}>Rewind</Flex>
+        <Flex
+          as={motion.div}
+          whileHover={{ scale: 1.1, color: "black" }}
+          onClick={resetHandler}
+          w="150px"
+          h="60px"
+          align="center"
+          justify="center"
+          border="1px solid"
+          cursor="pointer"
+        >
+          Reset
+        </Flex>
+        <Flex
+          as={motion.div}
+          whileHover={{ scale: 1.1, color: "black" }}
+          onClick={rewindHandler}
+          w="150px"
+          h="60px"
+          align="center"
+          justify="center"
+          border="1px solid"
+          cursor="pointer"
+        >
+          Rewind
+        </Flex>
       </Flex>
     </Flex>
   );
 };
+
+export default BouncingLoader;
+`;
+}
 
 export default BouncingLoader;
