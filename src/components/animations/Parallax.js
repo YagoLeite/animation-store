19b29d6eda3432025fdeb8d67552869302f1484a
Yago Batista <yagobatistafis@yahoo.com.br>
2023-11-22 16:14:48 -0300

    test

--- src/components/animations/Parallax.js
+++ src/components/animations/Parallax.js
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 import { Flex, Box, Text } from "@chakra-ui/react";
 import { CiWifiOn, CiBatteryFull, CiLock } from "react-icons/ci";
 import { RxLetterCaseToggle, RxReload } from "react-icons/rx";
