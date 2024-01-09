c74e9dc9b4ab06d4bd44974b638c51e8af176a67
Yago Batista <yagobatistafis@yahoo.com.br>
2024-01-09 00:19:45 -0300

    update: add new to scrollaway.

--- src/pages/[tag]/index.js
+++ src/pages/[tag]/index.js
@@ -127,7 +127,7 @@ export const getServerSideProps = async (context) => {
       description:
         "Discover an interactive card experience with our OverlayCard component, designed for seamless integration into modern web interfaces. Crafted using Chakra UI for sleek, responsive layouts and Framer Motion for fluid, intuitive animations, this component captivates with hover-triggered overlays that reveal additional content. Elevate your website with this dynamic, animated card that responds to user interaction, perfect for showcasing images with engaging informational overlays.",
       image: "https://animation-store.vercel.app/overlayCardImage.png",
-      tags: ["all", "scroll", "card"],
+      tags: ["all", "scroll", "card", "new"],
       key: Math.random(),
     },
     {
